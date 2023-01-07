62005b3f322c0c4134b00db4b2b55fed53a1124b
Jens B <jens.bachmayer@gmx.de>
2023-01-07 09:21:24 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+import postcssNesting from 'postcss-nesting';
+import postcssCustomMedia from 'postcss-custom-media';
+
+// https://vitejs.dev/config/
+// https://vitejs.dev/guide/build.html#public-base-path
+// https://www.youtube.com/watch?v=Sgcfiow4fVQ
+
+export default (args) => {
+  const isProduction = args.mode === 'production';
+  const generateScopedName = isProduction
+    ? '[hash:base64:3]'
+    : '[local]_[hash:base64:3]';
+
+  // console.log(`Running ${args.mode} mode ...\n`);
+  // console.log(args);
+
+  return defineConfig({
+    base: './',
+    plugins: [react()],
+    css: {
+      modules: {
+        localsConvention: 'camelCase',
+        generateScopedName,
+      },
+      postcss: {
+        plugins: [postcssCustomMedia(), postcssNesting()],
+      },
+    },
+  });
+};
